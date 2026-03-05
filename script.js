12034298d41843362ace71fac8aac769b4a3f0e3
Kamcio <kamciosiamsio@gmail.com>
2026-03-05 12:33:58 +0100

    Usprawnienie filtrowania

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
-console.log("działa")
-const filtr = document.getElementById("filtr");
-const items = document.querySelectorAll(".lista li");
+document.addEventListener("DOMContentLoaded", function () {
+        console.log("działa")
+        // Get references to input and list
+        const filtr = document.getElementById('filtr');
+        const lista = document.querySelectorAll('.lista li');
 
-filtr.addEventListener('input', function() {
-    const filtrujtekst = this.value.trim().toLowerCase();
+        // Listen for input changes
+        filtr.addEventListener('input', function () {
+        const filterText = this.value.trim().toLowerCase();
 
-    items.forEach(item => {
-        const text = item.textContent.toLowerCase();
-        item.classList.toggle('hidden', !text.includes(filtrujtekst))
+        // Loop through list items and toggle visibility
+        lista.forEach(item => {
+            const text = item.textContent.toLowerCase();
+            item.classList.toggle('hidden', !text.includes(filterText));
+        });
     });
 });
