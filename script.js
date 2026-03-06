6852ab26e9d804949f7dc57a7da14a34908e57ba
Kamcio <kamciosiamsio@gmail.com>
2026-03-06 11:03:34 +0100

    deleted:    filtrowanie.html 	modified:   formularz.css 	modified:   index.html 	modified:   koszyk.css 	modified:   koszyk.html 	modified:   logowanie.html 	modified:   main.css 	new file:   naglowek.css 	modified:   rejestracja.html 	modified:   script.js 	modified:   user.css 	modified:   uzytkownik.html 	modified:   zamowienie.css 	modified:   zamowienie.html

--- script.js
+++ script.js
@@ -4,24 +4,47 @@ document.addEventListener("DOMContentLoaded", function () {
         const filtr = document.getElementById('filtr');
         const lista = document.querySelectorAll('.lista li');
         const marka = document.getElementById('marka');
+        const kat = document.getElementById('kat');
+        const rodz = document.getElementById('rodz');
 
         // Listen for input changes
-        filtr.addEventListener('input', function () {
-        const filtrujtekst = this.value.trim().toLowerCase();
+        filtr.addEventListener('input', function () 
+        {
+            const filtrujtekst = this.value.trim().toLowerCase();
 
-        marka.addEventListener('input', function () {
-            const filtrujmarki = this.value;
-        })
-
-        // Loop through list items and toggle visibility
-        lista.forEach(item => {
-            const text = item.textContent.toLowerCase();
-            item.classList.toggle('hidden', !text.includes(filtrujtekst));
-
-        lista.forEach(item => {
-            const marka = item.textContent;
-            item.classList.toggle('hidden', !marka.includes(filtrujmarki));
-        })
+            // Loop through list items and toggle visibility
+            lista.forEach(item => 
+            {
+                const text = item.textContent.toLowerCase();
+                item.classList.toggle('hidden', !text.includes(filtrujtekst));
+            });
         });
-    });
+        marka.addEventListener('input', function () 
+        {
+            const filtrujmarki = this.value;
+            lista.forEach(item => 
+            {
+                const marka = item.textContent;
+                item.classList.toggle('hidden', !marka.includes(filtrujmarki));
+            });
+        });
+        kat.addEventListener('input', function() 
+        {
+            const filtrujkat = this.value;
+            lista.forEach(item => 
+            {
+                const kat = item.textContent;
+                item.classList.toggle('hidden', !kat.includes(filtrujkat));
+            });
+        });
+        rodz.addEventListener('input', function() 
+        {
+            const filtrujrodz = this.value;
+            lista.forEach(item => 
+            {
+                const rodz = item.textContent;
+                item.classList.toggle('hidden', !rodz.includes(filtrujrodz));
+            });
+        });
+
 });
